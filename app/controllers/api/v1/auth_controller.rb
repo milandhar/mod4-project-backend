d13f192c1ecb96e14c169405312982807363ea17
Milan <milan.dhar@gmail.com>
2019-07-09 12:15:21 -0700

    fixed auth controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,13 +4,11 @@ class Api::V1::AuthController < ApplicationController
   def create
     @user = User.find_by(username: user_login_params[:username])
     #User#authenticate comes from BCrypt
-    byebug
     if @user && @user.authenticate(user_login_params[:password])
       # encode token comes from ApplicationController
-      byebug
       token = encode_token({user_id: @user.id})
-
       render json: { user: UserSerializer.new(@user), jwt: token}, status: :accepted
+      return
     else
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
